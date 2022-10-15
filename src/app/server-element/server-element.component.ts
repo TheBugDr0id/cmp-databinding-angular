a224f76f15839cda9e6a343c5bd4bb582c386d54
ANUJ BHASIN <anujbhasin25@gmail.com>
2022-10-15 17:24:56 +0530

    Added an Alias to Custom Property

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: { type: string; name: string; content: string }; //element's type is JS Pbject
+  @Input('srvElement') element: { type: string; name: string; content: string }; //element's type is JS Pbject
 
   constructor() {}
 
